2cf46e14e49eab95cf8d26cbc101c40ca16912df
Thor <thor@10.14.6>
2019-11-20 01:03:23 +0100

    first task

--- src/week47/ReadWords.java
+++ src/week47/ReadWords.java
@@ -11,47 +11,20 @@ public class ReadWords {
 	
 	Scanner sc;
 	File fh;
-	Set<String> words;
-	HashMap<String, Integer> wordfreq;
-	LinkedList<String> verbs;
-	LinkedList<String> nouns;
-	LinkedList<String> adverbs;
-	LinkedList<String> prepos;
-	LinkedList<String> konj;
 	
 	public void readWordsIntoList(String filename) {
 		int tmpval = 0;
-		words = new HashSet<String>();
-		wordfreq = new HashMap< String, Integer>();
+		/*
+		words = words from file
+		wordfreq = map of word and frequency in file
+		*/
 		fh = new File(filename);
-		try {
-			
-			sc = new Scanner(fh);
-			while(sc.hasNextLine()) {
-				String line = sc.nextLine();
-				String[] lineArr = line.split(" ");
-				for (String s:lineArr) {
-					//s = s.replaceAll("[.-_,'!\"]","");
-					words.add(s);
-					try {
-						tmpval = wordfreq.get(s);
-						tmpval++;
-						wordfreq.put(s,tmpval);
-					} catch (Exception e) {
-						tmpval = 1;
-						wordfreq.put(s,tmpval);
-					}
-				}
-				
-			}
-		} catch (Exception e) {
-			System.out.println("Error");
+		sc = new Scanner(fh);
+		while(sc.hasNextLine()) {
+			//TODO:write code to fill the two collections
 		}
-		System.out.println("Size " + words.size());
-		System.out.println("Size " + wordfreq.size());
-		//System.out.println("Map" + wordfreq);
-		
 	}
-	
-	
+	System.out.println("Size " + words.size());
+	System.out.println("Size " + wordfreq.size());
 }
+
